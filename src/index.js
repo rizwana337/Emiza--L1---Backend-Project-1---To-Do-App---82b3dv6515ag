5b53f97bf2d4e921a5a15e9f118d9d5019d1c628
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-05-13 14:08:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,15 +5,48 @@ const seedWithDummyData = require('../seeder');
 
 dotenv.config();
 
-//connect to DB
+// Connect to database
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 
-const url = process.env.DATABASE_URL || "mongodb://localhost:27017/users";
-mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true }, () => {
-    console.log('connected to DB');
-    seedWithDummyData();
+const url = process.env.DATABASE_URL || 'mongodb://localhost:27017/users';
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
+  .then(() => {
+    console.log('Connected to database');
+    seedWithDummyData();
+  })
+  .catch(error => {
+    console.log('Error connecting to database:', error);
+  });
+
+// Start server
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 
-app.listen(3000, () => console.log('Server running......'));
+// const app = require('./app');
+// const dotenv = require('dotenv');
+// const mongoose = require('mongoose');
+// const seedWithDummyData = require('../seeder');
+
+// dotenv.config();
+
+// //connect to DB
+// mongoose.set('useCreateIndex', true);
+// mongoose.set('useFindAndModify', false);
+
+// const url = process.env.DATABASE_URL || "mongodb://localhost:27017/users";
+// mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true }, () => {
+//     console.log('connected to DB');
+//     seedWithDummyData();
+// })
+
+
+// app.listen(3000, () => console.log('Server running......'));
