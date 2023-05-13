c4a53811b8c631cbe2058f452d0ca07b9903e318
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-05-13 14:05:44 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
-const router = require('../routes/index'); 
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-//Router Middlewares
-app.use(express.json());
+const userRoutes = require('./routes/userRoutes');
 
-app.use('/api/v1', router);
+app.use(bodyParser.json());
+
+app.use('/api/v1/user', userRoutes);
 
 module.exports = app;
+
+
+// const express = require('express');
+// const app = express();
+// const router = require('../routes/index'); 
+
+// //Router Middlewares
+// app.use(express.json());
+
+// app.use('/api/v1', router);
+
+// module.exports = app;
 
