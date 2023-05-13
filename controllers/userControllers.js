f30e68005ce2f770791f3b2cf0af6fdd8b6a47bf
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-05-13 14:19:34 +0530

    Update userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,58 +1,92 @@
-const User = require('../models/user');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const JWT_SECRET = "newtonSchool";
-const saltRounds = 10;
 
+// Signup user
 exports.signupUser = async (req, res) => {
-  try {
-    const { email, password, name, role } = req.body;
+  const { email, password, name, role } = req.body;
 
-    const existingUser = await User.findOne({ email: email });
-    if (existingUser) {
-      return res.status(409).json({ message: 'User with given Email already registered', status: 'fail' });
+  try {
+    // Check if user already exists
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(409).json({
+        message: "User with given Email already registered",
+        status: "fail",
+      });
     }
 
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
 
+    // Create a new user
     const newUser = new User({
-      email: email,
+      email,
       password: hashedPassword,
-      name: name,
-      role: role
+      name,
+      role,
     });
+
+    // Save the user to the database
     await newUser.save();
 
-    res.status(200).json({ message: 'User SignedUp successfully', status: 'success' });
+    // Return success response
+    return res.status(200).json({
+      message: "User SignedUp successfully",
+      status: "success",
+    });
   } catch (error) {
-    res.status(404).json({ message: 'Something went wrong', status: 'fail' });
+    console.log(error);
+    return res.status(404).json({
+      message: "Something went wrong",
+      status: "fail",
+    });
   }
-}
+};
 
+// Login user
 exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
   try {
-    const { email, password } = req.body;
-
-    const existingUser = await User.findOne({ email: email });
-    if (!existingUser) {
-      return res.status(404).json({ message: 'User with this E-mail does not exist !!', status: 'fail' });
+    // Find user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        message: "User with this E-mail does not exist !!",
+        status: "fail",
+      });
     }
 
-    const match = await bcrypt.compare(password, existingUser.password);
-    if (!match) {
-      return res.status(403).json({ message: 'Invalid Password, try again !!', status: 'fail' });
+    // Compare password
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(403).json({
+        message: "Invalid Password, try again !!",
+        status: "fail",
+      });
     }
 
-    const token = jwt.sign({ userId: existingUser._id }, JWT_SECRET, { expiresIn: '1h' });
+    // Generate JWT token
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: "1h",
+    });
 
-    res.status(200).json({ status: 'success', token: token });
+    // Return success response
+    return res.status(200).json({
+      status: "success",
+      token,
+    });
   } catch (error) {
-    res.status(404).json({ message: 'Something went wrong', status: 'fail' });
+    console.log(error);
+    return res.status(404).json({
+      message: "Something went wrong",
+      status: "fail",
+    });
   }
-}
-
-module.exports = { loginUser , signupUser };
+};
 
 
 // const Users   = require("../models/user.js");
