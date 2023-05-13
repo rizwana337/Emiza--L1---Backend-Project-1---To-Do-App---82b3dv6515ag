1b1286acc558d9ecfb39ead90fc10dd3cfd41d6c
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-05-13 14:25:29 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,26 +1,15 @@
+
+
+
+
+
 const express = require("express");
+
+const { loginUser, signupUser } = require("../controllers/userControllers");
+
 const router = express.Router();
 
-const userController = require("../controllers/userController");
-
-// Signup route
-router.post("/signup", userController.signupUser);
-
-// Login route
-router.post("/login", userController.loginUser);
+router.get("/login", loginUser);
+router.post("/signup", signupUser)
 
 module.exports = router;
-
-
-
-
-// const express = require("express");
-
-// const { loginUser, signupUser } = require("../controllers/userControllers");
-
-// const router = express.Router();
-
-// router.get("/login", loginUser);
-// router.post("/signup", signupUser)
-
-// module.exports = router;
